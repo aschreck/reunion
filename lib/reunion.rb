3ad2a7f2bbe8047e13163c33de74d615e45b4e17
Aaron Schreck <schreck.aaron@gmail.com>
2017-09-13 09:57:05 -0600

    Add total cost functionality for the reunion

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -4,11 +4,15 @@ class Reunion
     
     def initialize(location, activities)
         @location = location
-        @activities = []
+        @activities = [activities]
     end 
 
     def add_activity(activity)
         @activities << activity
-        
     end 
+
+    def total_cost
+        @activities.inject(0) {|sum, activity| sum + activity.total_cost}
+    end 
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -4,25 +4,33 @@ require 'minitest/autorun'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
-    def setup
-        #fencing = Activity.new('Fencing', {'Joe' => 30, 'Sally' => 40, "Bill" => 50}, 120)
-        reunion = Reunion.new("ballroom")
-    end 
+    
 
     def test_reunion_has_a_location
-        reunion = setup
+        fencing = Activity.new('Fencing', {'Joe' => 30, 'Sally' => 40, "Bill" => 50}, 120)
+        reunion = Reunion.new("ballroom", fencing)
 
         assert_equal 'ballroom', reunion.location
     end
 
     def test_reunion_can_add_activities
-        reunion = setup
         fencing = Activity.new('Fencing', {'Joe' => 30, 'Sally' => 40, "Bill" => 50}, 120)        
         boating = Activity.new('Boating', {'Joe' => 30, 'Sally' => 40, "Bill" => 50}, 120)
+        reunion = Reunion.new("ballroom", fencing)
+        
         reunion.add_activity(boating)
-        reunion.add_activity(fencing)
         
         assert_equal 2, reunion.activities.length
+    end 
+
+    def test_reunion_can_add_its_total_cost
+        fencing = Activity.new('Fencing', {'Joe' => 30, 'Sally' => 40, "Bill" => 50}, 120)        
+        boating = Activity.new('Boating', {'Joe' => 30, 'Sally' => 40, "Bill" => 50}, 120)
+        reunion = Reunion.new("ballroom", fencing)
+        reunion.add_activity(boating)
+
+        assert_equal 240, reunion.total_cost
+
     end 
 end
 
